6a8da419fbbc5a581100d12aa7ed3ac451e7bc17
marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2022-11-17 18:32:29 +0000

    Change python exercise 2

--- trim1/ut2/a2/programa2.py
+++ trim1/ut2/a2/programa2.py
@@ -4,39 +4,27 @@ import math
 try:
 
 #Punto de inicio
-    num1 = float(sys.argv[1])
-    num2 = float(sys.argv[2])
+    x1 = float(sys.argv[1])
+    y1 = float(sys.argv[2])
 
 
 #Puntos cual de los dos es más cercano
-    num3 = float(sys.argv[3])
-    num4 = float(sys.argv[4])
-    num5 = float(sys.argv[5])
-    num6 = float(sys.argv[6])
+    x2 = float(sys.argv[3])
+    y2 = float(sys.argv[4])
+    x3 = float(sys.argv[5])
+    y3 = float(sys.argv[6])
+
+#Realizamos el calculo
+    d1 = (math.sqrt((x1-x2)**2+(y1-y2)**2))
+    d2 = (math.sqrt((x1-x3)**2+(y1-y3)**2))
+    
+#Comprobamos el resultado entre los dos para ver cual es mayor
+    if d1 < d2:
+        print(f'El punto más cercano es ({int(x2)},{int(y2)}) y está a una distancia de {d1}')
+    else:
+        print(f'El punto más cercano es ({int(x3)},{int(y3)}) y está a una distancia de {d2}')
 
 
-    def distancia(x1,y1,x2,y2):
-        d = (math.sqrt((x1-x2)**2+(y1-y2)**2))
-        return d
-
-#Restamos uno por luego se lo vamos a sumar dentro del bucle por si el numero es igual
-    inicio = (num1+num2)-1
-    fin1 = num3+num4
-    fin2 = num5+num6
-
-    while True:
-        inicio += 1
-        if inicio == fin1:
-            cercano = f"({int(num3)},{int(num4)})"
-            resultado = distancia(num1,num2,num3,num4)
-            break
-        elif inicio == fin2:
-            cercano = f"({int(num5)},{int(num6)})"
-            resultado = distancia(num1,num2,num5,num6)
-            break
-
-
-    print(f'El punto más cercano es {cercano} y está a una distancia de {resultado}')
 
 except:
     print("No has puesto ningun argumento")
