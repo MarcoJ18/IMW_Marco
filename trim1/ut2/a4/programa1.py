e9019a0e0b98ffd4933ad003cc154cb75023b42e
Marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2022-11-28 13:21:51 +0000

    Update exercise ut2-a4 programa1

--- trim1/ut2/a4/programa1.py
+++ trim1/ut2/a4/programa1.py
@@ -1,6 +1,6 @@
 import sys
 
-letra = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
+letra ='TRWAGMYFPDXBNJZSQVHLCKE'
 
 def dni(num):
     restoD = num % 23
